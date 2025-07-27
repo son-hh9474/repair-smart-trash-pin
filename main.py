2cefeed06325267162f2aa42d551e11b02228dc5
son-hh9474 <huynhhongson131@gmail.com>
2025-07-27 20:08:16 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from smart_trash_bin import SmartTrashBin
 
 if __name__ == "__main__":
-    trash_bin = SmartTrashBin(trigger_distance=2.0, hold_time=2)
+    trash_bin = SmartTrashBin(trigger_distance=2.0, hold_time=5)
 
     trash_bin.run()
